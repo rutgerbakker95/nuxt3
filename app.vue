50bc117882a2f7229be82579c0f959f4092d0023
Rutger Bakker <rutger7bakker@gmail.com>
2022-04-05 14:02:38 +0200

    initial

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('hello world!')
+  },
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+})
